2b82f409ce9a5149ea590f711157a3db3805e7fd
furkanonder <furkanonder@protonmail.com>
2022-11-08 17:37:48 +0300

    Add testcases for QueryManager

--- tests/test_glass_jar.py
+++ tests/test_glass_jar.py
@@ -17,7 +17,7 @@ class Car(Model):
     year: int
 
 
-class TestJar(unittest.TestCase):
+class TestBase(unittest.TestCase):
     def setUp(self):
         with shelve.open("database.jar", writeback=True) as db:
             db["tables"] = {}
@@ -25,6 +25,8 @@ class TestJar(unittest.TestCase):
     def tearDown(self):
         pathlib.Path("database.jar").unlink()
 
+
+class TestModel(TestBase):
     def test_save(self):
         item_obj = Item(name="test item", count=20).save()
         self.assertEqual(item_obj.name, "test item")
@@ -34,20 +36,6 @@ class TestJar(unittest.TestCase):
         self.assertEqual(car_obj.brand, "Tesla")
         self.assertEqual(car_obj.model, "Model S")
         self.assertEqual(car_obj.year, 2022)
-
-    def test_get(self):
-        item_obj = Item(name="test item", count=20).save()
-        item_db_obj = Item.records.get(id=1)
-        self.assertEqual(item_db_obj.id, item_obj.id)
-        self.assertEqual(item_db_obj.name, item_obj.name)
-        self.assertEqual(item_db_obj.count, item_obj.count)
-
-        car_obj = Car(brand="Tesla", model="Model S", year=2022).save()
-        car_db_obj = Car.records.get(id=1)
-        self.assertEqual(car_db_obj.id, car_obj.id)
-        self.assertEqual(car_db_obj.brand, car_obj.brand)
-        self.assertEqual(car_db_obj.model, car_obj.model)
-        self.assertEqual(car_db_obj.year, car_obj.year)
 
     def test_update(self):
         car_db_obj = Car(brand="Tesla", model="Model S", year=2022).save()
@@ -76,3 +64,47 @@ class TestJar(unittest.TestCase):
     def test_type_check(self):
         with self.assertRaises(TypeError):
             Car(brand="Tesla", model="Model S", year=2022.4)
+
+
+class TestQuery(TestBase):
+    def test_get(self):
+        item_obj = Item(name="test item", count=20).save()
+        item_db_obj = Item.records.get(id=1)
+        self.assertEqual(item_db_obj.id, item_obj.id)
+        self.assertEqual(item_db_obj.name, item_obj.name)
+        self.assertEqual(item_db_obj.count, item_obj.count)
+
+        car_obj = Car(brand="Tesla", model="Model S", year=2022).save()
+        car_db_obj = Car.records.get(id=1)
+        self.assertEqual(car_db_obj.id, car_obj.id)
+        self.assertEqual(car_db_obj.brand, car_obj.brand)
+        self.assertEqual(car_db_obj.model, car_obj.model)
+        self.assertEqual(car_db_obj.year, car_obj.year)
+
+    def test_all(self):
+        for _ in range(10):
+            Item(name="test item", count=20).save()
+        self.assertEqual(len(Item.records.all()), 10)
+
+    def test_first(self):
+        item_list = []
+
+        for _ in range(10):
+            item = Item(name="test item", count=20).save()
+            item_list.append(item)
+
+        self.assertEqual(Item.records.first().id, item_list[0].id)
+
+    def test_last(self):
+        item_list = []
+
+        for _ in range(10):
+            item = Item(name="test item", count=20).save()
+            item_list.append(item)
+
+        self.assertEqual(Item.records.last().id, item_list[-1].id)
+
+    def test_count(self):
+        for _ in range(5):
+            Item(name="test item", count=20).save()
+        self.assertEqual(Item.records.count(), 5)
