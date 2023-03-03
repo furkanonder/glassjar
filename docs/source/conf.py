f5b6a294aafa1009608be70a1abd33abc3ca687f
furkanonder <furkanonder@protonmail.com>
2023-03-04 02:16:23 +0300

    apply black sphinx conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'GlassJar'
-copyright = '2023, Furkan Onder'
-author = 'Furkan Onder'
-release = '0.2.0'
+project = "GlassJar"
+copyright = "2023, Furkan Onder"
+author = "Furkan Onder"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
